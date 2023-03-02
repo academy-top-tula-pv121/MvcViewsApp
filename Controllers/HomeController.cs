834f447ecfee615a26effe0f053962adfcae6e5d
maxefimov <efimov.top.academy@gmail.com>
2023-03-02 20:18:43 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcViewsApp.Models;
+using System.Diagnostics;
+
+namespace MvcViewsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            /*
+            ViewData
+            ViewBag
+            Model object
+            */
+
+            this.ViewData["title"] = "Home page";
+            ViewData["header"] = "Welcome to Home page";
+            ViewData["users"] = new List<string>() { "Bob", "Joe", "Tim", "Sam" };
+
+            ViewBag.Date = DateTime.Now.ToLongDateString();
+            ViewBag.Hour = DateTime.Now.Hour;
+            ViewBag.Users = new List<string>() { "Leo", "Tom", "Jim", "Sam" };
+
+            List<string> usersList = new List<string>() { "Max", "Ann", "Kim", "Pit" };
+
+            return View(usersList);
+        }
+
+        
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcViewsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
